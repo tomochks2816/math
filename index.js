612f2516cb624a66f0e29be3da11316aab9e3ce3
tomochks2816 <147356985+tomochks2816@users.noreply.github.com>
2024-07-09 21:24:08 +0900

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const server = http.createServer();
 const app = express(server);
 const __dirname = process.cwd();
 const bareServer = createBareServer('/bare/');
-const PORT = 8000;
+const PORT = 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
